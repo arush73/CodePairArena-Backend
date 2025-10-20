3cf497d8cd27c63ccc82aada5146a367dd93f6d8
Arush Choudhary <arushgzbin@gmail.com>
2025-10-20 08:30:24 +0530

    nothing much

--- temp.js
+++
@@ -1,3 +0,0 @@
-const str = `const fs = require('fs');\n\n// Reading input from stdin (using fs to read all input)\nconst input = fs.readFileSync(0, 'utf-8').trim();\nconst [a, b] = input.split(' ').map(Number);\n\nconsole.log(a + b);`
-
-console.log(str)
