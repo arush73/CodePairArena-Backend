de18f4c895202af769221d01cc19bd6a86785662
Arush Choudhary <arushgzbin@gmail.com>
2025-10-25 22:59:52 +0530

    fixed submisisons conrollers 2

--- src/controllers/submission.controllers.js
+++ src/controllers/submission.controllers.js
@@ -127,7 +127,7 @@ const createSubmission = asyncHandler(async (req, res) => {
   if (!allPasses) {
     submission = await Submission.create({
       problemId,
-      userId: "68ece11c90c73b758e20492d",
+      userId: req.user._id,
       sourceCode: code,
       language: language,
       status: "WRONG ANSWER",
